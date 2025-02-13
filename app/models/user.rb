2692674fb75956c4ff4813b8962e079f9eedb362
gabriel acer <gabrielaug4@gmail.com>
2025-02-13 02:07:23 -0300

    feat: generate initial user model with devise

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { administrador: 0, atendente: 1, funcionario: 2 }
+end
--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,6 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+end

b9be31041b2eaeb996a8dbf67531aa6d5f26f61e
gabriel acer <gabrielaug4@gmail.com>
2025-02-15 16:34:51 -0300

    raw: install carrierwave gem

--- app/models/visitante.rb
+++ app/models/visitante.rb
@@ -1,4 +1,7 @@
 class Visitante < ApplicationRecord
+  mount_uploader :foto, FotoUploader
+
+  belongs_to :unidade
   has_many :visitas
   validates :cpf, presence: true, uniqueness: true
   validates :nome, presence: true
